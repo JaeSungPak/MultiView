41198a35d78a3bbf7041eed09198b0d089b5cd0a
JaeSungPak <70958612+JaeSungPak@users.noreply.github.com>
2023-08-29 11:18:45 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,9 +16,11 @@ setup(
         "omegaconf>=2.3.0",
         "onnxruntime>=1.15.1",
         "onnx>=1.14.0",
-        "segment_anything @ git+https://github.com/facebookresearch/segment-anything.git"
         "rembg>=2.0.50",
         "trimesh>=3.23.1",
+    ],
+    dependency_links = [
+        "git+https://github.com/facebookresearch/segment-anything.git"
     ],
     author="jsp",
 )

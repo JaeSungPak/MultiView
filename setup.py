e6c7655112d43dbffb12c31c79016691b573611f
JaeSungPak <70958612+JaeSungPak@users.noreply.github.com>
2023-08-29 10:41:44 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,15 +8,15 @@ setup(
         "ldm",
         "ldm.models.diffusion",
         "ldm.modules.diffusionmodules",
-        "onnxruntime>=1.15.1",
-        "onnx>=1.14.0",
-        "git+https://github.com/facebookresearch/segment-anything.git",
     ],
     install_requires=[
         "diffusers>=0.19.3",
         "transformers>=4.31.0",
         "einops>=0.6.1",
         "omegaconf>=2.3.0",
+        "onnxruntime>=1.15.1",
+        "onnx>=1.14.0",
+        "git+https://github.com/facebookresearch/segment-anything.git",
     ],
     author="jsp",
 )
